f9a4033065d798d364a25698db4baed7c48981bc
eunjinjeong <eunjin.jeong.5115@gmail.com>
2023-12-21 19:28:58 +0900

    Future Builder로 UI 구현

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -10,7 +10,8 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   final ImagePicker _picker = ImagePicker(); //객체선언
-  List<XFile>? images; // ImagePicker로 여러 장의 사진을 가져와서 XFile이라는 형태로 리스트에 담긴다. (처음엔 사진이 없는 상태니까 ? 허용)
+  List<XFile>?
+      images; // ImagePicker로 여러 장의 사진을 가져와서 XFile이라는 형태로 리스트에 담긴다. (처음엔 사진이 없는 상태니까 ? 허용)
 
   @override
   void initState() {
@@ -27,9 +28,25 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('나의 액자'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('안드로이드 픽셀 이미지 앨범'),
+        ),
+        body: images == null
+            ? Center(
+                child:Text('이미지가 없습니다'),
+              )
+            : FutureBuilder(
+                future: images![0].readAsBytes(), // 데이터를 읽는 함수
+                builder: (context, snapshot) {
+                  final data = snapshot.data;
+                  if (data == null ||
+                      snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  return Image.memory(
+                    data, // 매개변수 (이미지 데이터를 나타내는 바이트 배열 -> 반드시 Unit8List 혹은 List<int>)
+                    width: double.infinity
+                  );
+                }));
   }
 }
